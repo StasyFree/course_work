8173a69cbeebc1636df1da5cf629f07c19a02f8a
Anastasia <aburlakova45@gmail.com>
2023-07-09 12:51:44 +0300

    функция change_date

--- src/operations.py
+++ src/operations.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Operation:
     def __init__(self, state, date, amount, currency, description, recipient, destination=None):
         self.state = state
@@ -7,4 +10,9 @@ class Operation:
         self.description = description
         self.destination = destination
         self.recipient = recipient
-        
+
+
+    def change_date(self):
+        self.date = datetime.datetime.strptime(self.date, "%Y-%m-%dT%H:%M:%S.%f")
+        return self.date
+
