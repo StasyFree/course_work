fb9b5ad6a8530763051f1fbfd2a3783cd51129a2
Anastasia <aburlakova45@gmail.com>
2023-07-09 19:10:52 +0300

    функция change_format_date

--- src/operations.py
+++ src/operations.py
@@ -11,8 +11,10 @@ class Operation:
         self.destination = destination
         self.recipient = recipient
 
-
     def change_date(self):
         self.date = datetime.datetime.strptime(self.date, "%Y-%m-%dT%H:%M:%S.%f")
         return self.date
 
+    def change_format_date(self):
+        self.date = datetime.datetime.strftime(self.date, "%d.%m.%Y")
+        return self.date
